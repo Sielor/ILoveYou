0f65995cd395fb066fb7b6d5e5654ab76183a17d
Itay Zenkin <itayzenkin11@gmail.com>
2026-02-21 16:50:11 +0000

    chore:linting using stylistic

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin"
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
@@ -11,4 +12,13 @@ export default defineConfig([
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
   { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  { plugins:
+    {
+      '@stylistic': stylistic
+    },
+  rules:
+    {
+      '@stylistic/indent': ['error', 2],
+    }
+  }
 ]);
